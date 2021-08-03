d4b2d76c3feeacc56ed6bfe2effd5d1b7776e5db
BetaHuhn <schiller@mxis.ch>
2021-08-03 10:56:12 +0200

    Add context menu

--- src/background.js
+++ src/background.js
@@ -14,6 +14,54 @@ chrome.omnibox.onInputEntered.addListener((input) => {
 	})
 })
 
+chrome.contextMenus.onClicked.addListener(({ menuItemId }) => {
+	chrome.storage.local.get((items) => {
+		const detaInstance = items.detaInstance
+
+		if (menuItemId === 'view-links') {
+			chrome.tabs.create({
+				url: detaInstance
+			})
+		} else if (menuItemId === 'report-bug') {
+			chrome.tabs.create({
+				url: 'https://github.com/WebCrateApp/feedback/issues/new/choose'
+			})
+		} else if (menuItemId === 'open-docs') {
+			chrome.tabs.create({
+				url: 'https://webcrate.app/docs'
+			})
+		} else if (menuItemId === 'open-settings') {
+			chrome.runtime.openOptionsPage()
+		}
+
+	})
+})
+
+/* context menu */
+chrome.contextMenus.create({
+	title: 'View links',
+	id: 'view-links',
+	contexts: [ 'browser_action' ]
+})
+
+chrome.contextMenus.create({
+	title: 'Open docs',
+	id: 'open-docs',
+	contexts: [ 'browser_action' ]
+})
+
+chrome.contextMenus.create({
+	title: 'Report bug',
+	id: 'report-bug',
+	contexts: [ 'browser_action' ]
+})
+
+chrome.contextMenus.create({
+	title: 'Open settings',
+	id: 'open-settings',
+	contexts: [ 'browser_action' ]
+})
+
 // Show settings page after install
 chrome.runtime.onInstalled.addListener((details) => {
 	if (details.reason === 'install') {
